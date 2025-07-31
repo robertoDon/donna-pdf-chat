4543e4e4662b02df07eddd04eea701d72209691f
robertoDon <roberto@don.adm.br>
2025-07-31 14:54:19 -0300

    Fix LangChain deprecation warnings by updating imports

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -6,8 +6,8 @@ import streamlit as st
 import numpy as np
 import faiss
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
 from langchain.schema import Document
 import replicate
 
